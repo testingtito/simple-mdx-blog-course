c8e39c8c0b4f7ff9bbaebe6b3e53ce265d381002
Toby Chee <testingtito@gmail.com>
2020-07-16 10:03:53 -0700

    Add gatby mad plugin and sharp library

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,39 @@
 module.exports = {
   plugins: [
     {
-      resolve:`gatsby-source-filesystem`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name:`pages`,
-        path:`${__dirname}/src/pages`
+        name: `pages`,
+        path: `${__dirname}/src/pages`
       },
     },
     {
-      resolve:`gatsby-source-filesystem`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        posts:`pages`,
-        path:`${__dirname}/src/posts`
+        posts: `pages`,
+        path: `${__dirname}/src/posts`
       },
     },
     {
-      resolve:`gatsby-source-filesystem`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name:`images`,
-        path:`${__dirname}/src/images`
+        name: `images`,
+        path: `${__dirname}/src/images`
+      },
+    },
+    `gatsby-plugin-sharp`, `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
       },
     },
   ],
